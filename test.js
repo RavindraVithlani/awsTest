8475b02b9313d181f68ddd1e7c80232dabf58366
RavindraVithlani <129835051+RavindraVithlani@users.noreply.github.com>
2024-02-09 14:07:02 +0530

    Update test.js
    first commit

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
   // Send the response body as "Hello, World!"
-  res.end('Hello, World!\n');
+  res.end('Hello\n');
 });
 
 // Define the port to listen on
